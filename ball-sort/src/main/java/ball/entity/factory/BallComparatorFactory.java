ff2cfc581f2bce0a0b704e57024924954572deb9
Lelikbjmak <denissolovey990@gmail.com>
2023-03-05 00:02:19 +0300

    Divide ComparatorTypes to ASC & DESC, hence build ASC/DESC BallComparator

--- ball-sort/src/main/java/ball/entity/factory/BallComparatorFactory.java
+++ ball-sort/src/main/java/ball/entity/factory/BallComparatorFactory.java
@@ -8,25 +8,48 @@ import ball.entity.balls.Ball;
 import java.util.Comparator;
 import java.util.function.Function;
 
+/**
+ * BillComparatorFactory - build a custom comparator to sort Basket with {@link Ball}
+ */
 public class BallComparatorFactory {
 
+    /**
+     * Build a custom single {@link BallComparator}
+     *
+     * @see Function
+     */
     public static Function<ComparatorType, BallComparator> getBallComparator = (comparatorType) -> {
 
         switch (comparatorType) {
-            case WEIGHT -> {
-                return BallComparatorUtils::compareByWeight;
+            case ASC_WEIGHT -> {
+                return BallComparatorUtils::compareByWeightAsc;
             }
-            case TYPE -> {
-                return BallComparatorUtils::compareByType;
+            case DESC_WEIGHT -> {
+                return BallComparatorUtils::compareByWeightDesc;
             }
-            case NAME -> {
-                return BallComparatorUtils::compareByName;
+            case ASC_TYPE -> {
+                return BallComparatorUtils::compareByTypeAsc;
             }
-            case COLOR -> {
-                return BallComparatorUtils::compareByColor;
+            case DESC_TYPE -> {
+                return BallComparatorUtils::compareByTypeDesc;
             }
-            case CIRCUMFERENCE -> {
-                return BallComparatorUtils::compareByCircumference;
+            case ASC_NAME -> {
+                return BallComparatorUtils::compareByNameAsc;
+            }
+            case DESC_NAME -> {
+                return BallComparatorUtils::compareByNameDesc;
+            }
+            case ASC_COLOR -> {
+                return BallComparatorUtils::compareByColorAsc;
+            }
+            case DESC_COLOR -> {
+                return BallComparatorUtils::compareByColorDesc;
+            }
+            case ASC_CIRCUMFERENCE -> {
+                return BallComparatorUtils::compareByCircumferenceAsc;
+            }
+            case DESC_CIRCUMFERENCE -> {
+                return BallComparatorUtils::compareByCircumferenceDesc;
             }
             default -> {
                 return Ball::compareTo;
@@ -34,15 +57,18 @@ public class BallComparatorFactory {
         }
     };
 
-    public static BallComparator getBallComparatorChain(ComparatorType... comparatorTypes) {
+    /**
+     * @param comparatorTypes - attributes to sort {@link ComparatorType}
+     * @method getBallComparatorChain - create a ComparatorChain
+     */
+    public static Comparator<Ball> getBallComparatorChain(ComparatorType... comparatorTypes) {
 
-        if (comparatorTypes.length == 0)
-            return Ball::compareTo;
+        if (comparatorTypes.length == 0) return Ball::compareTo;
 
-        BallComparator ballComparatorChain = getBallComparator.apply(comparatorTypes[0]);
+        Comparator<Ball> ballComparatorChain = getBallComparator.apply(comparatorTypes[0]);
 
         for (int i = 1; i < comparatorTypes.length; i++) {
-            ballComparatorChain = (BallComparator) ballComparatorChain.thenComparing(getBallComparator.apply(comparatorTypes[i]));
+            ballComparatorChain = ballComparatorChain.thenComparing(getBallComparator.apply(comparatorTypes[i]));
         }
 
         return ballComparatorChain;
