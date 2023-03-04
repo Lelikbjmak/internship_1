3e0e4316c883923007b5d0fe4630d6979e0a61b6
Lelikbjmak <denissolovey990@gmail.com>
2023-03-04 23:56:20 +0300

    Divide types to: ASC & DESC

--- ball-sort/src/main/java/ball/entity/ball_comparator/ComparatorType.java
+++ ball-sort/src/main/java/ball/entity/ball_comparator/ComparatorType.java
@@ -1,9 +1,20 @@
 package ball.entity.ball_comparator;
 
+/**
+ * @see java.lang.Enum
+ * Contributes to build custom {@link BallComparator},
+ * particularly business logic of sort alorithm (asc/desc).
+ * Choose sorting attributes.
+ */
 public enum ComparatorType {
-    NAME,
-    WEIGHT,
-    COLOR,
-    TYPE,
-    CIRCUMFERENCE
+    ASC_NAME,
+    DESC_NAME,
+    ASC_WEIGHT,
+    DESC_WEIGHT,
+    ASC_COLOR,
+    DESC_COLOR,
+    ASC_TYPE,
+    DESC_TYPE,
+    ASC_CIRCUMFERENCE,
+    DESC_CIRCUMFERENCE
 }
