2fcf34bc4b499ff34ffe7f0d60f5689c660e96d1
Lelikbjmak <denissolovey990@gmail.com>
2023-03-04 22:13:19 +0300

    FootballBall entity

---
+++ ball-sort/src/main/java/ball/entity/balls/BaseballBall.java
@@ -0,0 +1,23 @@
+package ball.entity.balls;
+
+import ball.entity.types.Color;
+import ball.entity.types.Type;
+
+public class BaseballBall extends Ball {
+
+    public BaseballBall(String name, double circumference,
+                        double weight, Color color) {
+
+        super(Type.BASEBALL);
+        this.name = name;
+        this.circumference = circumference;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "\nName: " + name + "\nType: " + type.name() + "\nColor: "
+                + color.name() + "\nWeight: " + weight + "\nCircumference: " + circumference;
+    }
+}
