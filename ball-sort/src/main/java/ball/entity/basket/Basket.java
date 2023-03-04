4604879126065113676099327ec52e65542984bf
Lelikbjmak <denissolovey990@gmail.com>
2023-03-04 22:14:51 +0300

    Basket

---
+++ ball-sort/src/main/java/ball/entity/basket/Basket.java
@@ -0,0 +1,27 @@
+package ball.entity.basket;
+
+import ball.entity.balls.Ball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket {
+
+    private final ArrayList<Ball> balls;
+
+    public Basket() {
+        balls = new ArrayList<>();
+    }
+
+    public void addBall(Ball ball) {
+        balls.add(ball);
+    }
+
+    public void addSeveralBalls(List<Ball> ballList) {
+        balls.addAll(ballList);
+    }
+
+    public List<Ball> getBalls() {
+        return this.balls;
+    }
+}
