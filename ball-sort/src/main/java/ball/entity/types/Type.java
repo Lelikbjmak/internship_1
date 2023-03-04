ae38be50798bb6d11b846642c212cea1731df232
Lelikbjmak <denissolovey990@gmail.com>
2023-03-04 22:15:31 +0300

    Type enum

---
+++ ball-sort/src/main/java/ball/entity/types/Type.java
@@ -0,0 +1,14 @@
+package ball.entity.types;
+
+public enum Type {
+
+    BASKETBALL,
+    FOOTBALL,
+    VOLLEYBALL,
+    BASEBALL,
+    SOCCER,
+    HANDBALL,
+    GOLF,
+    WATER_POLO,
+    TENNIS
+}
