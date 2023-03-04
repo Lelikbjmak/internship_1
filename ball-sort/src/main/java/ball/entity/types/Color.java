32bb98c486ea48a70080f35fe7a6561098ad8577
Lelikbjmak <denissolovey990@gmail.com>
2023-03-04 22:15:23 +0300

    Color enum

---
+++ ball-sort/src/main/java/ball/entity/types/Color.java
@@ -0,0 +1,16 @@
+package ball.entity.types;
+
+public enum Color {
+
+    RED,
+    GREEN,
+    BLUE,
+    PINK,
+    YELLOW,
+    ORANGE,
+    BLACK,
+    GREY,
+    BROWN,
+    PURPLE,
+    WHITE
+}
