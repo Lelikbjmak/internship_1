b58f8b4bb0d3f3154edaa3fc10bd9a2ddc7d5446
Lelikbjmak <denissolovey990@gmail.com>
2023-03-05 23:14:41 +0300

    Color Priority builder. Produce custom priority for each color for further sort

---
+++ ball-sort/src/main/java/ball/util/ColorPriorityBuilder.java
@@ -0,0 +1,55 @@
+package ball.util;
+
+import ball.entity.types.Color;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class ColorPriorityBuilder {
+
+    public ColorPriorityBuilder() {
+    }
+
+    public Map<Color, Integer> getEqualPriorityColorMap() {
+        Map<Color, Integer> priorityColorMap = new HashMap<>();
+        for (Color color :
+                Color.values()) {
+            priorityColorMap.put(color, Color.values().length);
+        }
+        return priorityColorMap;
+    }
+
+    public Map<Color, Integer> getCustomColorPriorityMap(Map<Color, Integer> priorityColorMap) {
+        Map<Color, Integer> colorIntegerMap = getEqualPriorityColorMap();
+        priorityColorMap.forEach(colorIntegerMap::replace);
+        return colorIntegerMap;
+    }
+
+    public Map<Color, Integer> getDarkToLightColorPriorityMap() {
+        Map<Color, Integer> priorityColorMap = new HashMap<>();
+        for (Color color :
+                Color.values()) {
+            priorityColorMap.put(color, color.ordinal());
+        }
+        return priorityColorMap;
+    }
+
+    public Map<Color, Integer> getLightToColorPriorityMap() {
+        Map<Color, Integer> priorityColorMap = new HashMap<>();
+        for (Color color :
+                Color.values()) {
+            priorityColorMap.put(color, Color.values().length - color.ordinal());
+        }
+        return priorityColorMap;
+    }
+
+    public Map<Color, Integer> getRandomColorPriorityMap() {
+        Map<Color, Integer> priorityColorMap = new HashMap<>();
+        for (Color color :
+                Color.values()) {
+            priorityColorMap.put(color, new Random().nextInt(100 - 1) + 1);
+        }
+        return priorityColorMap;
+    }
+}
