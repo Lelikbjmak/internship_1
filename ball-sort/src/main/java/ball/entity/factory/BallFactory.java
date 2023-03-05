16480960bc7cce31f2efd478a7b7e6884a3d1274
Lelikbjmak <denissolovey990@gmail.com>
2023-03-05 18:42:31 +0300

    Implement adding a new produced Ball to less loaded Basket

--- ball-sort/src/main/java/ball/entity/factory/BallFactory.java
+++ ball-sort/src/main/java/ball/entity/factory/BallFactory.java
@@ -1,18 +1,19 @@
 package ball.entity.factory;
 
+import ball.dto.BallProductionRequest;
+import ball.entity.balls.Ball;
 import ball.entity.basket.Basket;
+import ball.mapper.BallRequestToBallMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BallFactory {
 
+    private long basketNumber = 0;
+
     public BallFactory() {
         this.baskets = new ArrayList<>();
-    }
-
-    public BallFactory(List<Basket> baskets) {
-        this.baskets = new ArrayList<>(baskets);
     }
 
     private final List<Basket> baskets;
@@ -23,5 +24,33 @@ public class BallFactory {
 
     public void addBasket(Basket basket) {
         this.baskets.add(basket);
+        increaseBasketNumber();
+    }
+
+    public long getBasketNumber() {
+        return basketNumber;
+    }
+
+    private void increaseBasketNumber() {
+        this.basketNumber++;
+    }
+
+    public Ball produceBall(BallProductionRequest ballProductionRequest) {
+        Ball producedBall = new BallRequestToBallMapper().apply(ballProductionRequest);
+        addBallToBasket(producedBall);
+        return producedBall;
+    }
+
+    public void addBallToBasket(Ball ball) {
+        this.baskets.get(lessLoadedBasketIndex()).addBall(ball);
+    }
+
+    public int lessLoadedBasketIndex() {
+        int lessLoadedBasket = this.baskets.stream().mapToInt(basket -> basket.getBalls().size()).min().getAsInt();
+        for (int i = 0; i < this.baskets.size(); i++) {
+            if (baskets.get(i).getBalls().size() == lessLoadedBasket)
+                return i;
+        }
+        return 0;
     }
 }
