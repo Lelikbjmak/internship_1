db23be9d38d622fa33aa92bfe55300d6547c74f6
Lelikbjmak <denissolovey990@gmail.com>
2023-03-06 20:47:02 +0300

    Add single Type Priority Comparators + add opportunity to add Color/Type Priority compares to ComparatorChain

--- ball-sort/src/main/java/ball/entity/factory/BallComparatorFactory.java
+++ ball-sort/src/main/java/ball/entity/factory/BallComparatorFactory.java
@@ -1,15 +1,18 @@
 package ball.entity.factory;
 
 import ball.entity.ball_comparator.BallComparator;
-import ball.util.BallComparatorUtils;
-import ball.util.ColorPriorityBuilder;
 import ball.entity.ball_comparator.ComparatorType;
 import ball.entity.balls.Ball;
 import ball.entity.types.Color;
+import ball.entity.types.Type;
+import ball.util.BallComparatorUtils;
+import ball.util.ColorPriorityBuilder;
+import ball.util.TypePriorityBuilder;
 
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
 
 /**
@@ -78,6 +81,20 @@ public class BallComparatorFactory {
         return ballComparatorChain;
     }
 
+    public static Comparator<Ball> addColorPriorityComparesToChain(Map<Color, Integer> colorIntegerMap, Comparator<Ball> ballComparator) {
+        ColorPriorityBuilder builder = new ColorPriorityBuilder();
+        BallComparatorUtils.setColorPriorityMap(builder.getCustomColorPriorityMap(colorIntegerMap));
+        ballComparator = ballComparator.thenComparing(BallComparatorUtils::compareByColorPriority);
+        return ballComparator;
+    }
+
+    public static Comparator<Ball> addTypePriorityComparesToChain(Map<Type, Integer> typeIntegerMap, Comparator<Ball> ballComparator) {
+        TypePriorityBuilder builder = new TypePriorityBuilder();
+        BallComparatorUtils.setTypePriorityMap(builder.getCustomTypePriorityMap(typeIntegerMap));
+        ballComparator = ballComparator.thenComparing(BallComparatorUtils::compareByTypePriority);
+        return ballComparator;
+    }
+
     private static Map<Color, Integer> getDefaultColorPriorityMap() {
         Map<Color, Integer> colorPriority = new HashMap<>();
         for (Color color :
@@ -107,7 +124,36 @@ public class BallComparatorFactory {
 
     public static BallComparator getLightToDarkColorPriorityBallComparator() {
         ColorPriorityBuilder builder = new ColorPriorityBuilder();
-        BallComparatorUtils.setColorPriorityMap(builder.getLightToColorPriorityMap());
+        BallComparatorUtils.setColorPriorityMap(builder.getLightToDarkColorPriorityMap());
         return BallComparatorUtils::compareByColorPriority;
+    }
+
+    public static BallComparator getToLargeSportTypePriorityBallComparator() {
+        TypePriorityBuilder builder = new TypePriorityBuilder();
+        BallComparatorUtils.setTypePriorityMap(builder.getTypePriorityMapBySportScaleAsc());
+        return BallComparatorUtils::compareByTypePriority;
+    }
+
+    public static BallComparator getToSmallSportTypePriorityBallComparator() {
+        TypePriorityBuilder builder = new TypePriorityBuilder();
+        BallComparatorUtils.setTypePriorityMap(builder.getTypePriorityMapBySportScaleDesc());
+        return BallComparatorUtils::compareByTypePriority;
+    }
+
+    public static BallComparator getRandomTypePriorityComparator() {
+        TypePriorityBuilder builder = new TypePriorityBuilder();
+        BallComparatorUtils.setTypePriorityMap(builder.getRandomTypePriorityMap());
+        return BallComparatorUtils::compareByTypePriority;
+    }
+
+    public static BallComparator getCustomTypePriorityComparator(Map<Type, Integer> typeIntegerMap) {
+        TypePriorityBuilder builder = new TypePriorityBuilder();
+        BallComparatorUtils.setTypePriorityMap(builder.getCustomTypePriorityMap(typeIntegerMap));
+        return BallComparatorUtils::compareByTypePriority;
+    }
+
+    public static Comparator<Ball> addCustomComparator(Comparator<Ball> newComparator, Comparator<Ball> currentComparator) {
+        currentComparator = currentComparator.thenComparing(newComparator);
+        return currentComparator;
     }
 }
