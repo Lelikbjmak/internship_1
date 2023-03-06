febe96e0f0e023b4da2189332d50982951d891ce
Lelikbjmak <denissolovey990@gmail.com>
2023-03-06 23:57:08 +0300

    add merge Sort

--- ball-sort/src/main/java/ball/service/SortingAlgorithm.java
+++ ball-sort/src/main/java/ball/service/SortingAlgorithm.java
@@ -1,6 +1,8 @@
-package ball.sorts;
+package ball.service;
 
 import ball.entity.balls.Ball;
+import ball.sorts.merge.MergeSort;
+import ball.sorts.quick.QuickSort;
 
 import java.util.Comparator;
 import java.util.List;
@@ -10,5 +12,9 @@ public class SortingAlgorithm {
 
     public static void quickSort(List<Ball> balls, Comparator<Ball> ballComparator) {
         QuickSort.sort(balls, ballComparator);
+    }
+
+    public static void mergeSort(List<Ball> balls, Comparator<Ball> ballComparator) {
+        MergeSort.mergeSort(balls, ballComparator);
     }
 }
