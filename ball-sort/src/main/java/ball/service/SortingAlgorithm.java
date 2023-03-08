5208f9f82a63699eb667b312cb180947f79c055b
Lelikbjmak <denissolovey990@gmail.com>
2023-03-08 11:32:42 +0300

    Add other sorts

--- ball-sort/src/main/java/ball/service/SortingAlgorithm.java
+++ ball-sort/src/main/java/ball/service/SortingAlgorithm.java
@@ -1,20 +1,48 @@
 package ball.service;
 
-import ball.entity.balls.Ball;
+import ball.sorts.binary_tree.TreeSort;
+import ball.sorts.heap.HeapSort;
 import ball.sorts.merge.MergeSort;
+import ball.sorts.odd_even.OddEvenSort;
 import ball.sorts.quick.QuickSort;
+import ball.sorts.quick.QuickSortParallel;
 
 import java.util.Comparator;
 import java.util.List;
 
-// TODO: Add other types of sorts
 public class SortingAlgorithm {
 
-    public static void quickSort(List<Ball> balls, Comparator<Ball> ballComparator) {
-        QuickSort.sort(balls, ballComparator);
+    private SortingAlgorithm() {
+        throw new IllegalArgumentException(this.getClass() + " is Util class.");
     }
 
-    public static void mergeSort(List<Ball> balls, Comparator<Ball> ballComparator) {
-        MergeSort.mergeSort(balls, ballComparator);
+    public static <T> void quickSort(List<T> objects, Comparator<T> comparator) {
+        QuickSort<T> quickSort = new QuickSort<>(comparator);
+        quickSort.sort(objects);
+    }
+
+    public static <T> void parallelQuickSort(List<T> objects, Comparator<T> comparator) {
+        QuickSortParallel<T> quickSortParallel = new QuickSortParallel<>(comparator);
+        quickSortParallel.quickSort(objects);
+    }
+
+    public static <T> void mergeSort(List<T> objects, Comparator<T> comparator) {
+        MergeSort<T> mergeSort = new MergeSort<>(comparator);
+        mergeSort.mergeSort(objects);
+    }
+
+    public static <T> void oddEvenSort(List<T> objects, Comparator<T> comparator) {
+        OddEvenSort<T> oddEvenSort = new OddEvenSort<>(comparator);
+        oddEvenSort.oddEvenSort(objects);
+    }
+
+    public static <T> void heapSort(List<T> objects, Comparator<T> comparator) {
+        HeapSort<T> heapSort = new HeapSort<>(comparator);
+        heapSort.heapSort(objects);
+    }
+
+    public static <T> void treeSort(List<T> objects, Comparator<T> comparator) {
+        TreeSort<T> treeSort = new TreeSort<>(comparator);
+        treeSort.treeSort(objects);
     }
 }
