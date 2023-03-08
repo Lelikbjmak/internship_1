c92f8ba3b855ece70572a06706af16d6dc0975fd
Denis Solovey <109223818+Lelikbjmak@users.noreply.github.com>
2023-03-08 21:25:08 +0300

    Delete ball-sort/src/main/java/ball directory

--- ball-sort/src/main/java/ball/dto/BallProductionRequest.java
+++
@@ -1,8 +0,0 @@
-package ball.dto;
-
-import ball.entity.types.Color;
-import ball.entity.types.Type;
-
-public record BallProductionRequest(String name, Type type, Color color,
-                                    Double weight, Double circumference) {
-}
