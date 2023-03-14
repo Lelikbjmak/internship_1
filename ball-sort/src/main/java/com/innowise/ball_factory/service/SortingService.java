1d76e0e55eff35b441604009d16fdf1e017fdc9f
Lelikbjmak <denissolovey990@gmail.com>
2023-03-14 20:40:26 +0300

    Add default implementation with default Comparator

--- ball-sort/src/main/java/com/innowise/ball_factory/service/SortingService.java
+++ ball-sort/src/main/java/com/innowise/ball_factory/service/SortingService.java
@@ -1,13 +1,17 @@
 package com.innowise.ball_factory.service;
 
-import com.innowise.ball_factory.sorts.Sortable;
+import com.innowise.ball_factory.sorts.Sorter;
 
 import java.util.Comparator;
 import java.util.List;
 
 public class SortingService<T> {
 
-    public List<T> sort(Sortable<T> sortAlgorithm, List<T> objectList, Comparator<T> objectComparator) {
+    public List<T> sort(Sorter<T> sortAlgorithm, List<T> objectList, Comparator<T> objectComparator) {
         return sortAlgorithm.sort(objectList, objectComparator);
+    }
+
+    public List<T> sort(Sorter<T> sortAlgorithm, List<T> objectList) {
+        return sortAlgorithm.sort(objectList, (o1, o2) -> 0);
     }
 }
